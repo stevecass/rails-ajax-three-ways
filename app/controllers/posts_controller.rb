fbf1276ef1e084dc116ba9986793c714a3254041
Steven Cassidy <steven.cassidy@gmail.com>
2015-05-25 21:39:53 -0400

    Add show action to posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: :desc)
+    @comment = Comment.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 end
